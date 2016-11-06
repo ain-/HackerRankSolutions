1ca25a9d6a130df4f3936b894042f215d6300abc
Ain <ain.salula@finestmedia.ee>
2016-11-06 06:14:11 +0200

    Arrays: Left Rotation solution

---
+++ CrackingTheCodingInterview/DataStructures/ctci-array-left-rotation.py
@@ -0,0 +1,9 @@
+def array_left_rotation(a, n, d):
+    first_half = a[d:n]
+    second_half = a[0:d]
+    return first_half + second_half
+
+n, d = map(int, input().strip().split(' '))
+a = list(map(int, input().strip().split(' ')))
+answer = array_left_rotation(a, n, d);
+print(*answer, sep=' ')
