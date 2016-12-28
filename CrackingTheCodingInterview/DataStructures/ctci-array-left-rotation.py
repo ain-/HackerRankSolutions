e180c8e124980491298d7a4fc3b0da7f8126592d
Ain <ain.salula@gmail.com>
2016-12-28 08:23:50 +0200

    Arrays: Left Rotation - fix typo

--- CrackingTheCodingInterview/DataStructures/ctci-array-left-rotation.py
+++ CrackingTheCodingInterview/DataStructures/ctci-array-left-rotation.py
@@ -5,5 +5,5 @@ def array_left_rotation(a, n, d):
 
 n, d = map(int, input().strip().split(' '))
 a = list(map(int, input().strip().split(' ')))
-answer = array_left_rotation(a, n, d);
+answer = array_left_rotation(a, n, d)
 print(*answer, sep=' ')
